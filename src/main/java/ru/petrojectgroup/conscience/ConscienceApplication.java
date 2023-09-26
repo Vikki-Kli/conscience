7dcef5f73f22c309da13e4f78bc397a02249442c
Vikki-Kli <klimovaviktoriya265@gmail.com>
2023-09-26 22:48:56 +0300

    init

---
+++ src/main/java/ru/petrojectgroup/conscience/ConscienceApplication.java
@@ -0,0 +1,13 @@
+package ru.petrojectgroup.conscience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConscienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConscienceApplication.class, args);
+	}
+
+}
